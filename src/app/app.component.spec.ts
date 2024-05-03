01fbb45a44ee2748c22fe525c65848dddac68836
Rahul Kumar Chaurasiya <72122325+RahulRazj@users.noreply.github.com.>
2024-05-04 02:05:44 +0530

    fix: initial application setup & pre commit hooks setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('web app is running!');
+    expect(app.title).toEqual('Sql To Mongo');
   });
 });
